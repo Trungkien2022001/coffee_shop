1c1d620f2a4a881bf0c0a344680e681dd79e3d1a
trungkien2022001 <trungkien07yd@gmail.com>
2022-07-20 16:34:38 +0700

    hello

--- api/components/admin/menu/menuController.js
+++ api/components/admin/menu/menuController.js
@@ -1,15 +1,33 @@
 const { errorResponse, successResponse } = require("../../../helper/responseMessage")
 const { fetchGetAllMenu, fetchMenu, fetchCreateMenu, fetchUpdateMenu, fetchDeleteMenu, fetchCreateMenus } = require("./menuService")
-
+const redis = require('redis')
 async function getAllMenu(req, res){
+   
     const result = await fetchGetAllMenu(req, res)
     if(result.err) res.end(errorResponse(result))
-    else res.status(200).end(successResponse(result))
+    else {
+        let item = {
+            type: req.query.type, 
+            page: req.query.page
+        } 
+        item = JSON.stringify(item)
+        const page = req.query.page
+        const client = redis.createClient(6379)
+        await client.connect()
+        client.setEx(item, 3600, JSON.stringify(result))
+        res.status(200).end(successResponse(result))
+    }
 }
 async function getMenu(req, res){
     const result = await fetchMenu(req, res)
     if(result.err) res.end(errorResponse(result))
-    else res.status(200).end(successResponse(result))
+    else {
+        const id = req.query.id
+        const client = redis.createClient(6379)
+        await client.connect()
+        client.setEx(id, 3600, JSON.stringify(result))
+        res.status(200).end(successResponse(result))
+    }
 }
 async function createMenu(req, res){
     const result = await fetchCreateMenu(req, res)
